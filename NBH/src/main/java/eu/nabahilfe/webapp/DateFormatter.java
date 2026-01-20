35ecbdaeccd1562ccd6698ea7456c41a83296f34
Max <max@weissboeck.info>
2026-01-20 15:45:53 +0100

    fix date layout

--- NBH/src/main/java/eu/nabahilfe/webapp/DateFormatter.java
+++ NBH/src/main/java/eu/nabahilfe/webapp/DateFormatter.java
@@ -6,6 +6,10 @@ import java.util.Locale;
 
 public class DateFormatter {
 
+    /**
+     * @param date
+     * @return Date String with German Format yyyy.MM.dd for sorting in Tables
+     */
     public static String formatDE(LocalDate date) {
         if (date == null) return "";
         return date.format(
@@ -13,6 +17,23 @@ public class DateFormatter {
         );
     }
 
+
+    /**
+     * @param date
+     * @return Date String with German Format, not for sorting but for display in text fields
+     */
+    public static String formatReverseDE(LocalDate date) {
+        if (date == null) return "";
+        return date.format(
+            DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale.GERMAN)
+        );
+    }
+
+
+    /**
+     * @param date
+     * @return Date String with ISO Format yyyy-MM-dd for use with date type in html form
+     */
     public static String formatISO(LocalDate date) {
         if (date == null) return "";
         return date.toString();
