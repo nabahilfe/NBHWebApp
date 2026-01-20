f398e5a21f888c241a5e9ed06de6a98b8caacd62
Max <max@weissboeck.info>
2026-01-20 15:15:15 +0100

    book-accountable page displaying data is ok now

--- NBH/src/main/java/eu/nabahilfe/webapp/DateFormatter.java
+++ NBH/src/main/java/eu/nabahilfe/webapp/DateFormatter.java
@@ -5,11 +5,18 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class DateFormatter {
+
     public static String formatDE(LocalDate date) {
         if (date == null) return "";
         return date.format(
             DateTimeFormatter.ofPattern("yyyy.MM.dd", Locale.GERMAN)
         );
     }
+
+    public static String formatISO(LocalDate date) {
+        if (date == null) return "";
+        return date.toString();
+    }
+
 }
 
