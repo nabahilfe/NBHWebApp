9c44958e0f014c829e76fdb72db18f05c4905998
Max <max@weissboeck.info>
2026-01-22 15:26:23 +0100

    passt

--- NBH/src/main/java/eu/nabahilfe/webapp/accountings/Accountable.java
+++ NBH/src/main/java/eu/nabahilfe/webapp/accountings/Accountable.java
@@ -3,13 +3,15 @@ package eu.nabahilfe.webapp.accountings;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import eu.nabahilfe.webapp.members.Member;
+
 /**
  * Interface für alles was verbucht wird (Zeitscheck kauf, Mitgliedschaft...)
  */
 public interface Accountable {
     public abstract String getAccountableClass();       // MemberFee, TimeCheque, Transaction, ...
     public abstract Long getAccountableId();
-    public abstract Long getAccountableMemberId();      // Nur bei Zeitscheck-Kauf oder Mitgliedsgebühr relevant, sonst null
+    public abstract Member getAccountableMember();      // Nur bei Zeitscheck-Kauf oder Mitgliedsgebühr relevant, sonst null
     public abstract String getTransactionType();        // INCOME oder EXPENSE - muss aus Enum TransactionType kommen
     public abstract LocalDate getTransactionDate();
     public abstract BigDecimal getTransactionAmount();
