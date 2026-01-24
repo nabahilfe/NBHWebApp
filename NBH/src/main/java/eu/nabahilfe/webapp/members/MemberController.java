f88dfb6d4c2553e4e13e937f2685278bf145de67
Max <max@weissboeck.info>
2026-01-24 15:18:32 +0100

    feat: separate received and given time transfers in member detail view

--- NBH/src/main/java/eu/nabahilfe/webapp/members/MemberController.java
+++ NBH/src/main/java/eu/nabahilfe/webapp/members/MemberController.java
@@ -138,10 +138,8 @@ public class MemberController {
     @GetMapping("/{id}")
     String editMember(final Model model, @PathVariable Long id) {
         log.debug("Editing Member: {}", id);
-        model.addAttribute("timeTransfers", timeTransferRepository.findAllByOrderByDateOfServiceDesc());
-        log.debug("Loading timeTransfers for Member id={}", id);
-        model.addAttribute("timeTransfers", timeTransferRepository.findTop10ByFromMember_IdOrToMember_IdOrderByDateOfServiceDesc(id, id));
-        log.debug("Loading purchased TimeCheques for Member id={}", id);
+        model.addAttribute("receivedTimeTransfers", timeTransferRepository.findAllByToMember_IdOrderByDateOfServiceDesc(id));
+        model.addAttribute("givenTimeTransfers", timeTransferRepository.findAllByFromMember_IdOrderByDateOfServiceDesc(id));
         model.addAttribute("purchasedTimeCheques", timeCheckRepository.findAllByAssignedTo_IdOrderByTransactionDateDesc(id));
         return "members/detail-member";
     }
