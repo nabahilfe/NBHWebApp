c0ee3f5c00bc25907ace436f160f31fd066c2a4b
Max <max@weissboeck.info>
2026-04-14 15:31:55 +0200

    relocate pdf files

--- NBH/src/main/java/eu/nabahilfe/webapp/timecheques/TimeChequePdfPersonalizationService.java
+++ NBH/src/main/java/eu/nabahilfe/webapp/timecheques/TimeChequePdfPersonalizationService.java
@@ -10,21 +10,11 @@ import com.lowagie.text.pdf.PdfStamper;
 
 public class TimeChequePdfPersonalizationService {
 
-//	public TimeChequePdfPersonalizationService() {
-//
-//		try {
-//			personalizeTimeCheque("John", "123", new FileOutputStream("ZeitScheck_personalized.pdf"));
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
-
 
     public void personalizeTimeCheque(String memberName, String memberNumber, OutputStream output) throws IOException {
 
-        InputStream is = getClass().getResourceAsStream("/ZeitScheck.pdf");
-        if(is == null)
+        InputStream is = getClass().getResourceAsStream("/pdf/ZeitScheck.pdf");
+        if (is == null)
             throw new RuntimeException("ZeitScheck.pdf template not found!");
 
         PdfReader reader = new PdfReader(is);
