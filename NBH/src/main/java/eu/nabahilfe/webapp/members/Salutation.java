ad8cd0a7869aa3c3582ec4b31e70b9be98e2c689
Max <max@weissboeck.info>
2026-05-17 19:51:22 +0200

    add copyright and license notice

--- NBH/src/main/java/eu/nabahilfe/webapp/members/Salutation.java
+++ NBH/src/main/java/eu/nabahilfe/webapp/members/Salutation.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2025–2026 Maximilian Weißböck
+ * Licensed under the MIT License (see LICENSE file).
+ */
+
 package eu.nabahilfe.webapp.members;
 
 // see https://thorben-janssen.com/hibernate-enum-mappings/
