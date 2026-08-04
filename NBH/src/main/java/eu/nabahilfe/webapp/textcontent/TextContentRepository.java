d5380762556e79d74fd87ee1b550dde9f2688d4b
Maximilian Weißböck <max@weissboeck.info>
2026-08-04 15:19:13 +0200

    fix spring boot warnings

--- NBH/src/main/java/eu/nabahilfe/webapp/textcontent/TextContentRepository.java
+++ NBH/src/main/java/eu/nabahilfe/webapp/textcontent/TextContentRepository.java
@@ -8,9 +8,7 @@ package eu.nabahilfe.webapp.textcontent;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TextContentRepository extends JpaRepository<TextContent, Long> {
 
     // Find by element code (exact)
