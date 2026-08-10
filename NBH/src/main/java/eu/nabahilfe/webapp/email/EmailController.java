03e6a010eaddd5266bd680b18d3f6163f812f49a
Maximilian Weißböck <max@weissboeck.info>
2026-08-10 13:57:36 +0200

    fix potential security hole

--- NBH/src/main/java/eu/nabahilfe/webapp/email/EmailController.java
+++ NBH/src/main/java/eu/nabahilfe/webapp/email/EmailController.java
@@ -3,8 +3,9 @@ package eu.nabahilfe.webapp.email;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,7 +24,8 @@ public class EmailController {
         this.baseUrl = baseUrl;
     }
 
-    @GetMapping("/sendTestEmail")
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/sendTestEmail")
     public String sendTestEmail() {
         log.info("Sending test email...");
 
