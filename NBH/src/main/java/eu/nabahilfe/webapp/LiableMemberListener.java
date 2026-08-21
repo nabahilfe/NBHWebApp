2269ba349561481cf68694dbf2e3e1d16cfcbd1c
Maximilian Weißböck <max@weissboeck.info>
2026-08-21 11:47:19 +0200

    fix missing liableMemberName if no membership fee is accounted

--- NBH/src/main/java/eu/nabahilfe/webapp/LiableMemberListener.java
+++ NBH/src/main/java/eu/nabahilfe/webapp/LiableMemberListener.java
@@ -1,6 +1,7 @@
 package eu.nabahilfe.webapp;
 
 import eu.nabahilfe.webapp.accountings.Accountable;
+import eu.nabahilfe.webapp.accountings.AccountingEntry;
 import eu.nabahilfe.webapp.members.Member;
 import eu.nabahilfe.webapp.security.SecurityUtils;
 import jakarta.persistence.PrePersist;
@@ -16,18 +17,38 @@ public class LiableMemberListener {
     @PrePersist
     public void prePersist(Object entity) {
 
-        if (!(entity instanceof Accountable accountable)) {
-            return;
-        }
-
-        if (accountable.getLiableMemberName() != null) {
-            return;
-        }
-
         Member current = securityUtils.getCurrentUser();
 
-        if (current != null) {
-            accountable.setLiableMemberName(current.getName());
+        if (entity instanceof Accountable accountable) {
+
+	        if (accountable.getLiableMemberName() == null || accountable.getLiableMemberName().isEmpty()) {
+		        if (current != null) {
+		            accountable.setLiableMemberName(current.getName());
+		        }
+		        else {
+		            // Handle the case where current user is null, if necessary
+		            // For example, you might want to throw an exception or log a warning
+		            throw new IllegalStateException("Current user is not available for setting liable member name in Accountable.");
+		        }
+	        }
+	        return;
         }
+
+
+        if (entity instanceof AccountingEntry accEntry) {
+
+	        if (accEntry.getLiableMemberName() == null || accEntry.getLiableMemberName().isEmpty()) {
+		        if (current != null) {
+		        	accEntry.setLiableMemberName(current.getName());
+		        }
+		        else {
+		            // Handle the case where current user is null, if necessary
+		            // For example, you might want to throw an exception or log a warning
+		            throw new IllegalStateException("Current user is not available for setting liable member name in AccountingEntry.");
+		        }
+	        }
+	        return;
+        }
+
     }
 }
